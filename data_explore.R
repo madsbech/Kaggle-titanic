d2783346275474f7bb05d3e97aa5dd7c08a05e5c
madsbech <30551947+madsbech@users.noreply.github.com>
2017-09-25 16:54:00 +0200

    Update data_explore.R
    Survival rates by gender and class

--- data_explore.R
+++ data_explore.R
@@ -6,8 +6,12 @@ library(tidyverse)
 #Importing data files
 train <- read_csv("train.csv")
 
-
-
+#Shows ratio of survival in each Pclass
+ggplot(train,aes(x=Pclass,fill=as.character(Survived)))+
+  geom_bar()
+#Gender and class rates
+train %>% group_by(Pclass,Sex) %>% summarise(rate=mean(Survived),count=n())
+#Histogram of fares
 ggplot(train,aes(Fare))+
   geom_histogram(binwidth=5)+
   coord_cartesian(xlim=c(0,200))
