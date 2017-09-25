fcb6e4325a0f9b9ce2295cb523d6e388d458b8bf
madsbech <30551947+madsbech@users.noreply.github.com>
2017-09-25 16:43:52 +0200

    Add files via upload
    A first look

--- data_explore.R
+++ data_explore.R
@@ -6,6 +6,11 @@ library(tidyverse)
 #Importing data files
 train <- read_csv("train.csv")
 
+
+
+ggplot(train,aes(Fare))+
+  geom_histogram(binwidth=5)+
+  coord_cartesian(xlim=c(0,200))
 #Higher average fare among survivors
 train %>% group_by(Survived) %>% summarise(mean(Fare))
 #Women seem more likely to survive
